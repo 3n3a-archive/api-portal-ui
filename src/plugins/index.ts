3fd5d9d3673908a5966d1b1515401d65602dfe17
3n3a <46775561+3n3a@users.noreply.github.com>
2022-12-30 16:32:38 +0100

    Add head for managing title

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -10,14 +10,18 @@ import vuetify from './vuetify'
 import pinia from '../store'
 import router from '../router'
 import axios from './axios'
+import { createHead } from "@vueuse/head"
 
 // Types
 import type { App } from 'vue'
+
+const head = createHead()
 
 export function registerPlugins (app: App) {
   loadFonts()
   app
     .use(vuetify)
+    .use(head)
     .use(router)
     .use(pinia)
     .use(axios, {
