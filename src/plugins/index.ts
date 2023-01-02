75bf27af017f2b9906a0b50009d3c52a3f063ed7
3n3a <46775561+3n3a@users.noreply.github.com>
2023-01-03 00:43:17 +0100

    make the thing build

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -9,20 +9,16 @@ import { loadFonts } from './webfontloader'
 import vuetify from './vuetify'
 import pinia from '../store'
 import router from '../router'
-import { createHead } from "@vueuse/head"
 import Vue3EasyDataTable from 'vue3-easy-data-table';
 
 
 // Types
 import type { App } from 'vue'
 
-const head = createHead()
-
 export function registerPlugins (app: App) {
   loadFonts()
   app
     .use(vuetify)
-    .use(head)
     .use(router)
     .use(pinia)
     .component('EasyDataTable', Vue3EasyDataTable)
